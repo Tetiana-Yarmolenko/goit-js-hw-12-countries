652b04ec40da2ddad6e653fd60363506c79b1efb
Tetiana-Yarmolenko <kylik_t_86_86@ukr.net>
2020-11-10 18:50:23 +0200

    last

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './styles.css';
+// import itemsColection from '../menu.json';
+import pokemonCardTpl from "../template.hbs";
+
+import API from './js/api-servise';
+import getRefs from './js/get-refs';
+
+const refs = getRefs();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const searchQuery = form.elements.query.value;
+
+  API.fetchPokemon(searchQuery)
+    .then(renderPokemonCard)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+}
+
+function renderPokemonCard(pokemon) {
+  const markup = pokemonCardTpl(pokemon);
+  refs.cardContainer.innerHTML = markup;
+}
+
+function onFetchError(error) {
+  alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
+}
